3b40ee34122cd5e292f3fac289d321420f16a485
Johan Thoren <johan@thoren.xyz>
2023-07-16 14:44:51 +0200

    Don't run cli tests if not on macOS.

--- tests/cli.rs
+++ tests/cli.rs
@@ -2,6 +2,7 @@ use assert_cmd::prelude::*; // Add methods on commands
 use predicates::prelude::*; // Used for writing assertions
 use std::process::Command; // Run programs
 
+#[cfg(target_os = "macos")]
 #[test]
 fn test_cli_help() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("check_macos_updates")?;
@@ -16,6 +17,7 @@ fn test_cli_help() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+#[cfg(target_os = "macos")]
 #[test]
 fn test_cli_version() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("check_macos_updates")?;
